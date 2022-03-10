294a73d5072162238937a921c85ecd82f86e0b5a
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-03-10 09:33:40 -0300

    Utilizando Immer

--- projec-redux/src/store/index.ts
+++ projec-redux/src/store/index.ts
@@ -1,6 +1,13 @@
 import { createStore } from 'redux'
+import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
 
+
+export interface IState {
+
+        cart: ICartState;
+
+}
 /*
     Podemos pensar no store como o estado global do redux
 */
