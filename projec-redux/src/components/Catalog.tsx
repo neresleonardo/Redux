2c1373ec201698aed4aa23ba68456e031b477743
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-03-09 22:20:49 -0300

    actions

--- projec-redux/src/components/Catalog.tsx
+++ projec-redux/src/components/Catalog.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import api from '../services/api';
+import { addProductToCart } from '../store/modules/cart/action';
+import { IProduct } from '../store/modules/cart/types';
 
 
 const Catalog: React.FC = () => {
 
-    //Acesando o Store
-    // Podemos usa o useStore mas é melhor usar o userSelector 
-
-    const catalog = useSelector(state => state);
-
-    console.log(catalog);
+    const dispatch = useDispatch()
+    const [ catalog, setCatalog ] = useState<IProduct[]>([]);
     
-    
+    useEffect(() => {
+        api.get('products').then(response => {
+            setCatalog(response.data);
+        })
+    }, [])
+
+    const handleAddProductToCart = useCallback((product: IProduct) => {
+        dispatch(addProductToCart(product))
+    }, [dispatch]);
 
     return(
-        <h1>Catalog</h1>
+
+        <div>
+
+            <h1>Catalog</h1>
+    
+            {catalog.map(product => (
+              <article>
+                 <strong>{product.title}</strong> {"  - "}
+                 <span>{product.price}</span> {"  "}
+
+                 <button
+                  type="button"
+                  onClick={() => handleAddProductToCart(product)}
+
+                  >Comprar</button>
+              </article>  
+            ))}
+        </div>
     )
 }
 
