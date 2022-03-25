5d4ac4e8f889f7871d6d5524a1b2ef25bb293810
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-03-25 11:44:41 -0300

    Aumentando quantidade

--- projec-redux/src/store/index.ts
+++ projec-redux/src/store/index.ts
@@ -2,6 +2,7 @@ import { createStore } from 'redux'
 import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
 
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 export interface IState {
 
@@ -12,7 +13,10 @@ export interface IState {
     Podemos pensar no store como o estado global do redux
 */
 
-const store = createStore(rootReducer); // Valor inicial
+const store = createStore(
+    rootReducer,
+    composeWithDevTools()
+    ); // Valor inicial
 
 
 export default store;
--- projec-redux/src/store/modules/cart/reducer.ts
+++ projec-redux/src/store/modules/cart/reducer.ts
@@ -1,32 +1,36 @@
 import { Reducer } from "redux";
+import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
     items: []
 };
 
-const cart: Reducer<ICartState> = (state = INITIAL_STATE , action) => {
-    switch(action.type) {
-        case 'ADD_PRODUCT_TO_CART': {
-            const { product } = action.payload;
+    const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
+        return produce(state, draft => {
+            switch (action.type) {
+                case 'ADD_PRODUCT_TO_CART': {
+                    const { product } = action.payload;
 
-            return {
-                ...state,
-                items: [
-                    ...state.items,
-                    {
-                        product,
-                        quantity: 1
-                    }
-                ]
-            }
+                    const productInCartIndex = draft.items.findIndex(item => 
+                        item.product.id === product.id,
+                        );
 
-        }
-        default: {
-            return state;
-        }
+                        if (productInCartIndex > 0) {
+                            draft.items[productInCartIndex].quantity++;
+                        } else {
+                            draft.items.push({
+                                product,
+                                quantity: 1,
+                            });
+                        }
+                        break;
+                }
+            default: {
+        return draft;
+            }   
+            }   
+        });
     }
-
-}
 
 export default cart; 
