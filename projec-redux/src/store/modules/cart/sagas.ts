34ee90d034d70d794960270432a94653fc54c652
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-03-25 12:31:52 -0300

    Obtendo quantidade do carrinho

--- projec-redux/src/store/modules/cart/sagas.ts
+++ projec-redux/src/store/modules/cart/sagas.ts
@@ -1,7 +1,17 @@
-import { all, takeLatest } from 'redux-saga/effects'
+import { all, select, takeLatest } from 'redux-saga/effects'
+import { addProductToCart } from './action'
+import { IState } from '../..'
+type CheckProductStock = ReturnType<typeof addProductToCart>;
 
-function checkProductStock() {
-    console.log("add card");
+function* checkProductStock({payload}: CheckProductStock) {
+    const { product } = payload;
+    
+    const currentQuantity: number = yield select((state: IState) => {
+        return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+    })
+
+    console.log(currentQuantity);
+    
     
 }
 
